25293a6fc7c00ed38096a994ff1300500e5efa36
Isaac <isaacwildsecond@gmail.com>
2022-08-21 17:00:40 +0100

    Inital layout of site made

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import './style.css';
-import printMe from './print';
 
-printMe();
+const wrapper = document.createElement('div');
+wrapper.classList.add('wrapper');
+document.body.appendChild(wrapper);
+
+// title
+const txtTitle = document.createElement('p');
+txtTitle.textContent = 'BattleShip';
+txtTitle.classList.add('txtTitle');
+
+// content
+const txtContent = document.createElement('div');
+txtContent.textContent = 'game goes here!';
+txtContent.classList.add('txtContent');
+
+// footer
+const txtFooter = document.createElement('p');
+txtFooter.textContent = 'A game made by Isaac';
+txtFooter.classList.add('txtFooter');
+
+wrapper.append(txtTitle, txtContent, txtFooter);
