c4ea273fffdec060838f5b0e1d998efefe2b72ff
Isaac <isaacwildsecond@gmail.com>
2022-09-29 18:26:50 +0100

    Added factory function and tests for ship but last test is not working and always passes

--- src/ship.js
+++ src/ship.js
@@ -1,4 +1,23 @@
-function ship(a, b) {
-  return a + b;
-}
+// this is a factory function to create a ship object for the game.
+// needs to have the ships length, where they have been hit and if they are sunk or not.
+
+const ship = (length, gridSpaces) => {
+  const hitSpaces = [];
+  const hit = (hitGrid) => {
+    for (let i = 0; i < gridSpaces.length; i++) {
+      if (hitGrid === gridSpaces[i]) {
+        hitSpaces.push(hitGrid);
+      } else { console.log('miss'); }
+    }
+  };
+  const isSunk = () => {
+    if (length - 1 === hitSpaces.length) {
+      return true;
+    }
+    return false;
+  };
+  return {
+    length, gridSpaces, hitSpaces, hit, isSunk,
+  };
+};
 module.exports = ship;
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,5 +1,22 @@
 const ship = require('./ship');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(ship(1, 2)).toBe(3);
+test('Makes a ship of lenght 5', () => {
+  const ship1 = ship(5, ['A1', 'A2', 'A3', 'A4', 'A5']);
+  expect(ship1.length).toBe(5);
+});
+
+test('hit function adds to the hitSpaces correctly', () => {
+  const ship1 = ship(5, ['A1', 'A2', 'A3', 'A4', 'A5']);
+  ship1.hit('A1');
+  expect(ship1.hitSpaces[0]).toBe('A1');
+});
+
+test('when all spaces are hit ship sunk = true', () => {
+  const ship1 = ship(5, ['A1', 'A2', 'A3', 'A4', 'A5']);
+  ship1.hit('A1');
+  ship1.hit('A2');
+  ship1.hit('A3');
+  ship1.hit('A4');
+  // this is not working and should return test as fail but always returns true
+  expect(ship1.isSunk).toBeTruthy();
 });
