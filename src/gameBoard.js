ae52cd0882dc9c084f542a782c26b15d609c0e8b
Isaac <isaacwildsecond@gmail.com>
2022-10-17 17:17:36 +0100

    Corrected use of calling and creating game board in jest test

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-import-module-exports */
 /* eslint-disable max-len */
 /*
 This needs to a factory function that calls back to the ship factory function to place a ship at a specific coordinates.
@@ -7,9 +8,17 @@ This needs to a factory function that calls back to the ship factory function to
 import ship from './ship';
 
 const gameBoard = () => ({
+  shipLocations: [],
+  missedShot: [],
   placeShip(length, gridSpaces) {
+    // Needs to call back to Ship FF
     const newShip = ship(length, gridSpaces);
+    return newShip;
   },
+  receiveAttack(gridSpaces) {
+    // Needs to add to missed shots or connect to hit F in ship FF
+  },
+  allShipsSunk() {},
 });
 
 module.exports = gameBoard;
--- src/gameBoard.test.js
+++ src/gameBoard.test.js
@@ -1,6 +1,7 @@
 const gameBoard = require('./gameBoard');
 
 test('Makes a ship of lenght 3', () => {
-  gameBoard(3, ['A1', 'A2', 'A3']);
-  expect(newShip.length).toBe(3);
+  const playerBoard = gameBoard();
+  playerBoard.placeShip(3, ['A1', 'A2', 'A3']);
+  expect(playerBoard.newShip.length).toBe(3);
 });
